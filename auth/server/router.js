1507127d8c9636564d6ee6eccc953bc63979cb7b
long.le <xuanlongvts@gmail.com>
2017-05-09 22:46:13 +0700

    authentication

--- auth/server/router.js
+++ auth/server/router.js
@@ -5,7 +5,9 @@ const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', {
 	session: false
 });
-
+const requireSignin = passport.authenticate('local', {
+	session: false
+});
 
 module.exports = function(app){
 	app.get('/', requireAuth, function(req, res){
@@ -13,5 +15,6 @@ module.exports = function(app){
 			hi: 'there'
 		});
 	});
+	app.post('/signin', requireSignin, Authentication.signin);
 	app.post('/signup', Authentication.signup);
 };
